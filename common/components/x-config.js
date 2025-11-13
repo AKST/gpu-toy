deb7e9cf0747f22aa2fe76a3c3714007a6ec203d
akst <contact@akst.io>
2025-11-13 23:12:57 +1100

    pass data back and forth between iframe and parent

---
+++ common/components/x-config.js
@@ -0,0 +1,70 @@
+const createTextNode = document.createTextNode.bind(document);
+
+function createKnob(name, labelText, value) {
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  input.id = name;
+  input.name = name;
+  input.value = value;
+  label.htmlFor = name;
+  label.appendChild(createTextNode(labelText));
+  return [label, input];
+}
+
+export class ConfigSideBarElement extends HTMLElement {
+  #root;
+
+  constructor(){
+    super();
+    this.#root = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const cssLink = document.createElement('link')
+    cssLink.rel = 'stylesheet';
+    cssLink.href = import.meta.resolve('./x-config.css');
+
+    const form = this.form = document.createElement('form');
+    const fieldSet = this.fieldset = document.createElement('fieldset')
+    const fields = this.fields = document.createElement('div');
+    fields.className = 'fields';
+
+    const submit = document.createElement('input');
+    submit.type = 'submit';
+    submit.value = 'Submit';
+
+    const legend = document.createElement('legend');
+    legend.appendChild(createTextNode('Knobs'));
+    fieldSet.appendChild(legend);
+    fieldSet.appendChild(fields);
+    fieldSet.appendChild(submit);
+    form.appendChild(fieldSet);
+
+
+    this.#root.replaceChildren(cssLink, form);
+    this.flush();
+
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const formData = new FormData(form);
+      const detail = Object.fromEntries(formData.entries());
+      this.dispatchEvent(new CustomEvent('cfg-update', { detail, bubbles: true, composed: true }));
+    });
+  }
+
+  setKnobs(knobs) {
+    this.fieldset.style.display = 'block';
+    for (const { name, label: labelText, init } of knobs) {
+      const [label, input] = createKnob(name, labelText, init);
+      this.fields.appendChild(label);
+      this.fields.appendChild(input);
+    }
+  }
+
+  flush() {
+    this.fields.replaceChildren();
+    this.fieldset.style.display = 'none';
+  }
+}
+
+customElements.define("x-config", ConfigSideBarElement);
--- index.js
+++ index.js
@@ -2,16 +2,29 @@
  * @param {URL} url
  */
 export function loadApp({ searchParams }) {
+  const { attr, config, actions } = getChrome();
   const app = searchParams.get('example') ?? 'learn-001.html';
-  const attr = document.querySelector('#attribution');
   const iframe = document.querySelector('iframe');
   attr.flush?.();
+  config.flush?.();
+  actions.flush?.();
   iframe.src = `./examples/${app}`;
+}
+
+function getChrome() {
+  const attr = document.querySelector('#attribution');
+  const config = document.querySelector('#config');
+  const actions = document.querySelector('#actions');
+  return { attr, config, actions };
 }
 
 globalThis.addEventListener('click', event => {
   const { target } = event;
-  if (target instanceof HTMLAnchorElement) {
+  console.log(event);
+  if (
+      target instanceof HTMLAnchorElement &&
+      target.hostname === location.hostname
+  ) {
     event.preventDefault();
     const url = new URL(target.href);
     globalThis.history.pushState({}, '', url);
@@ -19,24 +32,35 @@ globalThis.addEventListener('click', event => {
   }
 });
 
+globalThis.addEventListener('cfg-update', event => {
+  const iframe = document.querySelector('iframe');
+  iframe.contentWindow.postMessage({
+    kind: 'knobs',
+    data: event.detail,
+  });
+});
+
 globalThis.addEventListener('message', event => {
+  const { attr, config, actions } = getChrome();
   const message = event.data;
-  const attr = document.querySelector('#attribution');
-  const actions = document.querySelector('#actions');
 
   switch (message.kind) {
     case 'attribute':
-      attr.setAttribution(message.title, message.link)
+      attr.setAttribution?.(message.title, message.link)
       break;
 
     case 'register-button':
-      actions.addButton(message.label, message.id);
+      actions.addButton?.(message.label, message.id);
+      break;
+
+    case 'register-knobs':
+      config.setKnobs?.(message.knobs);
       break;
 
     default:
+      console.warn('unknown message', message);
       break;
   }
 });
 
-console.clear();
 loadApp(new URL(globalThis.location.href));
