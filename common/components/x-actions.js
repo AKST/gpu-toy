09cb64d0ef66398566ecc3d45dfbbffb50bfba58
akst <contact@akst.io>
2025-11-15 04:05:53 +1100

    reasonable output

--- common/components/x-actions.js
+++ common/components/x-actions.js
@@ -1,17 +1,25 @@
+import { createStyleSheetLink } from './common/dom-kit.js';
+
 export class ActionsElement extends HTMLElement {
   #root = this.attachShadow({ mode: "open" });
 
   connectedCallback() {
+    const cssLinks = [
+      import.meta.resolve('./common/style-reset.css'),
+      import.meta.resolve('./x-actions.css'),
+    ].map(createStyleSheetLink)
+
     this.#root.addEventListener('click', event => {
       const iframe = document.querySelector('iframe');
       const button = event.target;
       const id = button.dataset.actionId;
       iframe.contentWindow.postMessage({ kind: 'action', id });
     });
+
+    this.#root.replaceChildren(...cssLinks);
   }
 
   addButton(label, id) {
-    this.flush();
     const button = document.createElement('button');
     button.appendChild(document.createTextNode(label));
     button.dataset.actionId = id;
